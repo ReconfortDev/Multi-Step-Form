dd84cdbeb35a460c4e25d8ce2cd09e0711b3023a
reconfortdev <reconfort.daniel@amalitechtraining.org>
2024-07-12 14:15:37 +0200

    finalising all issues

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 import {AsyncPipe, NgClass, NgForOf, NgIf, NgOptimizedImage} from "@angular/common";
 import {PersonalInfoComponent} from "./components/personal-info/personal-info.component";
 import {PlanComponent} from "./components/plan/plan.component";
@@ -11,96 +11,101 @@ import {ADDON, BILLING_PERIOD, PLAN} from "./model/FormInterface";
 import {BehaviorSubject} from "rxjs";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, NgOptimizedImage, PersonalInfoComponent, PlanComponent, AddonsComponent, SummaryComponent, FinishComponent, NgIf, NgForOf, NgClass, AsyncPipe],
-  templateUrl: './app.component.html',
+    selector: 'app-root',
+    standalone: true,
+    imports: [RouterOutlet, NgOptimizedImage, PersonalInfoComponent, PlanComponent, AddonsComponent, SummaryComponent, FinishComponent, NgIf, NgForOf, NgClass, AsyncPipe],
+    templateUrl: './app.component.html',
 })
 export class AppComponent {
-  currentStep: number = 1;
-  personalInfoForm: FormGroup;
+    currentStep: number = 4;
+    personalInfoForm: FormGroup;
 
-  plans: PLAN[] = [
-    { name: 'Arcade', monthlyPrice: 9, yearlyPrice: 90, icon: '/assets/images/icon-arcade.svg', active: true },
-    { name: 'Advanced', monthlyPrice: 12, yearlyPrice: 120, icon: '/assets/images/icon-advanced.svg', active: false },
-    { name: 'Pro', monthlyPrice: 15, yearlyPrice: 150, icon: '/assets/images/icon-pro.svg', active: false },
-  ];
+    plans: PLAN[] = [
+        {name: 'Arcade', monthlyPrice: 9, yearlyPrice: 90, icon: '/assets/images/icon-arcade.svg', active: true},
+        {name: 'Advanced', monthlyPrice: 12, yearlyPrice: 120, icon: '/assets/images/icon-advanced.svg', active: false},
+        {name: 'Pro', monthlyPrice: 15, yearlyPrice: 150, icon: '/assets/images/icon-pro.svg', active: false},
+    ];
 
-  addons: ADDON[] = [
-    { name: 'Online service', description: 'Access to multiplayer games', price: 1, selected: true },
-    { name: 'Cloud storage', description: 'Store your game saves securely', price: 2, selected: false },
-    { name: 'Premium support', description: 'Priority support for any issues', price: 3, selected: false },
-  ];
+    addons: ADDON[] = [
+        {name: 'Online service', description: 'Access to multiplayer games', price: 1, selected: true},
+        {name: 'Cloud storage', description: 'Store your game saves securely', price: 2, selected: false},
+        {name: 'Premium support', description: 'Priority support for any issues', price: 3, selected: false},
+    ];
 
-  billingPeriod$ = new BehaviorSubject<BILLING_PERIOD>('monthly');
+    billingPeriod$ = new BehaviorSubject<BILLING_PERIOD>('monthly');
 
-  // Personal info functionalities
+    // Personal info functionalities
 
-  // Function of validating form
-  phoneValidator(): ValidatorFn {
-    return (control: AbstractControl): ValidationErrors | null => {
-      const validPhoneNumberPattern = /^\+?[1-9]\d{1,14}$/;
-      const isValid = validPhoneNumberPattern.test(control.value);
-      return isValid ? null : { invalidPhone: true };
-    };
-  }
-
-  constructor(private fb: FormBuilder) {
-    this.personalInfoForm = this.fb.group({
-      name: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]],
-      phone: ['', [Validators.required, this.phoneValidator()]]
-    });
-  }
-
-  goToNextStep(): void {
-    if (this.currentStep === 1 && this.personalInfoForm.invalid) {
-      this.personalInfoForm.markAllAsTouched();
-      return;
+    // Function of validating form
+    phoneValidator(): ValidatorFn {
+        return (control: AbstractControl): ValidationErrors | null => {
+            const validPhoneNumberPattern = /^\+?[1-9]\d{1,14}$/;
+            const isValid = validPhoneNumberPattern.test(control.value);
+            return isValid ? null : {invalidPhone: true};
+        };
     }
-    if(this.currentStep < 5) {
-      this.currentStep++;
+
+    constructor(private fb: FormBuilder) {
+        this.personalInfoForm = this.fb.group({
+            name: ['', Validators.required],
+            email: ['', [Validators.required, Validators.email]],
+            phone: ['', [Validators.required, this.phoneValidator()]]
+        });
     }
-  }
 
-  goToPreviousStep(): void {
-    if(this.currentStep > 1) {
-      this.currentStep--;
+    goToNextStep(): void {
+        if (this.currentStep === 1 && this.personalInfoForm.invalid) {
+            this.personalInfoForm.markAllAsTouched();
+            return;
+        }
+        if (this.currentStep < 5) {
+            this.currentStep++;
+        }
     }
-  }
 
-  stepTitle(step: number): string {
-    switch (step) {
-      case 1:
-        return 'YOUR INFO';
-      case 2:
-        return 'SELECT PLAN';
-      case 3:
-        return 'ADD-ONS';
-      case 4:
-        return 'SUMMARY';
-      case 5:
-        return 'FINISH';
-      default:
-        return '';
+    goToPreviousStep(): void {
+        if (this.currentStep > 1) {
+            this.currentStep--;
+        }
     }
-  }
 
-  toggleBillingPeriod() {
-    const currentPeriod = this.billingPeriod$.getValue();
-    this.billingPeriod$.next(currentPeriod === 'monthly' ? 'yearly' : 'monthly');
-  }
+    stepTitle(step: number): string {
+        switch (step) {
+            case 1:
+                return 'YOUR INFO';
+            case 2:
+                return 'SELECT PLAN';
+            case 3:
+                return 'ADD-ONS';
+            case 4:
+                return 'SUMMARY';
+            case 5:
+                return 'FINISH';
+            default:
+                return '';
+        }
+    }
 
-  selectPlan(selectedPlan: PLAN) {
-    this.plans.forEach(plan => plan.active = plan === selectedPlan);
-  }
+    toggleBillingPeriod() {
+        const currentPeriod = this.billingPeriod$.getValue();
+        this.billingPeriod$.next(currentPeriod === 'monthly' ? 'yearly' : 'monthly');
+    }
 
-  toggleAddon(addon: ADDON) {
-    addon.selected = !addon.selected;
-  }
+    selectPlan(selectedPlan: PLAN) {
+        this.plans.forEach(plan => plan.active = plan === selectedPlan);
+    }
 
-  changePlan(){
-    this.currentStep = 2
-  }
+    getBillingPeriod(): BILLING_PERIOD {
+        return this.billingPeriod$.getValue();
+    }
+
+    toggleAddon(addon: ADDON) {
+        addon.selected = !addon.selected;
+    }
+
+    changePlan() {
+        this.currentStep = 2
+    }
+
 
 }
--- src/app/components/summary/summary.component.ts
+++ src/app/components/summary/summary.component.ts
@@ -1,13 +1,15 @@
 import {Component, Input} from '@angular/core';
-import {ADDON, PLAN} from "../../model/FormInterface";
-import {NgForOf, NgIf} from "@angular/common";
+import {ADDON, BILLING_PERIOD, PLAN} from "../../model/FormInterface";
+import {AsyncPipe, NgForOf, NgIf} from "@angular/common";
+import {BehaviorSubject} from "rxjs";
 
 @Component({
   selector: 'app-summary',
   standalone: true,
   imports: [
     NgIf,
-    NgForOf
+    NgForOf,
+    AsyncPipe
   ],
   templateUrl: './summary.component.html',
 })
@@ -15,6 +17,7 @@ export class SummaryComponent {
   @Input() changePlan!: () => void
   @Input() plans!: PLAN[];
   @Input() addons!: ADDON[];
+  @Input() billingPeriod!: BILLING_PERIOD;
 
   constructor() {}
 
@@ -27,11 +30,27 @@ export class SummaryComponent {
   }
 
   getTotal(): number {
-    let total = this.getActivePlan()?.monthlyPrice || 0;
+
+    let total = 0;
+
+    const activePlan = this.getActivePlan();
+    if (activePlan) {
+      if (this.billingPeriod === 'monthly') {
+        total += activePlan.monthlyPrice;
+      } else if (this.billingPeriod === 'yearly') {
+        total += activePlan.yearlyPrice;
+      }
+    }
+
     const selectedAddons = this.getActiveAddons();
     selectedAddons.forEach(addon => {
-      total += addon.price;
+      if (this.billingPeriod === 'yearly') {
+        total += addon.price * 12;
+      } else {
+        total += addon.price;
+      }
     });
-    return total
+
+    return total;
   }
 }
